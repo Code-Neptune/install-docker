1982ac2ec35e94c443f04f62b311ddecb140bfc1
Shashank Jadon <11077014+Code-Neptune@users.noreply.github.com>
2022-02-08 22:22:37 +0530

    Changed made to support ARM and AMD64 architecture

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,13 +1,9 @@
-#! /bin/bash
-
-sudo apt-get update
-
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+var_arch=$(dpkg --print-architecture) 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-echo "Docker Installed"
+sudo add-apt-repository "deb [arch=$var_arch] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+sudo apt install docker-compose -y
